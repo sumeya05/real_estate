4aaa4e31dbddbbacc95d9586b966085f2690992c
sumeya <sumeyabishar70gmail.com>
2026-06-08 15:34:29 +0300

    Initial commit

---
+++ real_estate/real_estate_management/doctype/maintenance_request/test_maintenance_request.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, sumeya bishar and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestMaintenanceRequest(FrappeTestCase):
+	pass
